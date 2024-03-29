09bab6e7cb55796e30a6141325bd1c68dc7c5131
Nikhil Gupta Ravva <nikhilguptaravva@Nikhils-MacBook-Air.local>
2024-03-29 21:38:24 +0530

    first time my swiggy is pushing to git

---
+++ backend/routes/firmRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const firmController = require('../controllers/firmControllers');
+const verifyTokens = require('../middelwares/verifyTokens');
+
+const router = express.Router();
+router.post('/add-firm',verifyTokens,firmController.addFirm);
+
+router.get('/uploads/:imageName',(req,res)=>{
+    const imageName = req.params.imageName;
+    res.headersSent('Content-Type','image/jpeg');
+    res.sendFile(path.join(__dirname,'..'/'uploads',imageName));
+})
+router.delete('/:firmId', firmController.deleteFirmById);
+
+module.exports = router;
